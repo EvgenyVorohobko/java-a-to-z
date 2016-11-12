fde6348f6cc81aa7a21fab8163e50267817b8f63
Evgeny <vorokhobko2011@yandex.ru>
2016-11-12 19:54:53 +0300

    add test

---
+++ chapter_001/src/test/java/by/vorokhobko/Calculate.java
@@ -0,0 +1,26 @@
+package by.vorokhobko;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * test
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru)
+ * @version 1
+ * @since 11.11.2016
+ */
+public class CalculateTest {
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenAddOneToOneThenTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(out.toString(), is("Hello World\r\n"));
+    }
+}
