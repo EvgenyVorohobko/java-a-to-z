e961df5d8747467b99f31b44c935e682dd22d17b
Evgeny <vorokhobko2011@yandex.ru>
2016-11-12 21:10:14 +0300

    mod

--- chapter_001/src/main/java/by/vorokhobko/Calculate.java
+++ chapter_001/src/main/java/by/vorokhobko/Calculate.java
@@ -1,17 +1,17 @@
-package by.vorokhobko;
+package by.vorokhobko; 
 
-/**
-* Класс Calculate решение задачи части 001, урок 1
-* @author vorokhobko
-* @since 11.11.2016
+/** 
+* Класс Calculate решение задачи части 001, урок 1. 
+* @author vorokhobko. 
+* @since 11.11.2016 
 */ 
 
-public class Calculate {
-	/**
-	 * Main
-	 * @param args - args
-	 */
-	public static void main(String[] args){
-		System.out.println("Hello world!");
-	}
-}
+public class Calculate { 
+	/** 
+	 * Main 
+	 * @param args - args 
+	 */ 
+	public static void main(String[] args){ 
+		System.out.println("Hello world!"); 
+	} 
+} 
