2fdaf333900de99c6cf8a99f3153556e87e19b1e
Evgeny <vorokhobko2011@yandex.ru>
2016-11-12 21:13:01 +0300

    mod

--- chapter_001/src/test/java/by/vorokhobko/Calculate.java
+++ chapter_001/src/test/java/by/vorokhobko/Calculate.java
@@ -1,27 +1,27 @@
-package by.vorokhobko; 
- 
-import org.junit.Test; 
-import java.io.ByteArrayOutputStream; 
-import java.io.PrintStream; 
-import static org.hamcrest.core.Is.is; 
-import static org.junit.Assert.assertThat; 
- 
-/** 
- * Test. 
- * 
- * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru). 
- * @version 1. 
- * @since 11.11.2016. 
- */ 
-public class CalculateTest { 
-    /** 
-     * Test add. 
-     */ 
-    @Test 
-    public void whenAddOneToOneThenTwo() { 
-        ByteArrayOutputStream out = new ByteArrayOutputStream(); 
-        System.setOut(new PrintStream(out)); 
-        Calculate.main(null); 
-        assertThat(out.toString(), is("Hello World\r\n")); 
-    } 
-} 
+package by.vorokhobko;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @version 1.
+ * @since 11.11.2016.
+ */
+public class CalculateTest {
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenAddOneToOneThenTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(out.toString(), is("Hello World\r\n"));
+    }
+}
