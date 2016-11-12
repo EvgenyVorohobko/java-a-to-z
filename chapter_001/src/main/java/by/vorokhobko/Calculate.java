7a536978a3cc44f87b70fa6a114c6058c4c08c5a
Evgeny <vorokhobko2011@yandex.ru>
2016-11-12 19:50:44 +0300

    add package

---
+++ chapter_001/src/main/java/by/vorokhobko/Calculate.java
@@ -0,0 +1,13 @@
+package vorokhobko;
+
+/**
+* Класс Calculate решение задачи части 001, урок 1
+* @author vorokhobko
+* @since 08.11.2016
+*/ 
+
+public class Calculate { 
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+	}
+}
