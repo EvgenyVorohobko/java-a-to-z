94ae88a0140389278fda55919b3e3875ed412c85
Evgeny <vorokhobko2011@yandex.ru>
2016-11-19 17:20:49 +0300

    add Calculator

---
+++ chapter_001/src/test/java/by/vorokhobko/CalculatorTest.java
@@ -0,0 +1,69 @@
+package by.vorokhobko;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+
+/**
+ * CalculatorTest.
+ *
+ * Class CalculatorTest for testing class Calculator.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 13.11.2016.
+ * @version 1.
+ */
+
+ public class CalculatorTest {
+	 /**
+	  * numberExeption.
+	  */
+	private final double numberExeption = 0.1;
+	 /**
+	  * Add test.
+	  */
+	public void testWhenPassArgToAddItShouldReturn() {
+		Calculator calc = new Calculator();
+		final double first = 2;
+		final double second = 2;
+		final double resultingNumber = 4;
+		calc.add(first, second);
+		double result = calc.getResult();
+		assertThat(result, is(closeTo(resultingNumber, numberExeption)));
+	  }
+	 /**
+	  * Take test.
+	  */
+	public void testWhenPassArgToTakeItShouldReturn() {
+		Calculator calc = new Calculator();
+		final double first = 4;
+		final double second = 2;
+		final double resultingNumber = 2;
+		calc.take(first, second);
+		double result = calc.getResult();
+		assertThat(result, is(closeTo(resultingNumber, numberExeption)));
+	  }
+	/**
+	 * Multiple test.
+	 */
+	public void testWhenPassArgToMultItShouldReturn() {
+		Calculator calc = new Calculator();
+		final double first = 3;
+		final double second = 2;
+		final double resultingNumber = 6;
+		calc.mult(first, second);
+		double result = calc.getResult();
+		assertThat(result, is(closeTo(resultingNumber, numberExeption)));
+	  }
+	/**
+	 * Sharing test.
+	 */
+	public void testWhenPassArgToSharItShouldReturn() {
+		Calculator calc = new Calculator();
+		final double first = 10;
+		final double second = 2;
+		final double resultingNumber = 5;
+		calc.shar(first, second);
+		double result = calc.getResult();
+		assertThat(result, is(closeTo(resultingNumber, numberExeption)));
+	  }
+ }
