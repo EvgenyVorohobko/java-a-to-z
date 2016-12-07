62d3389f9d3ab5ffb008d48a50b68b358bb13516
Evgeny <vorokhobko2011@yandex.ru>
2016-12-07 21:38:50 +0300

    mod Calculate

--- chapter_001/src/test/java/by/vorokhobko/CalculateTest.java
+++ chapter_001/src/test/java/by/vorokhobko/CalculateTest.java
@@ -23,6 +23,6 @@ public class CalculateTest {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
         Calculate.main(null);
-        assertThat(out.toString(), is("Hello World\n"));
+        assertThat(out.toString(), is("Hello World\r\n"));
     }
 }
