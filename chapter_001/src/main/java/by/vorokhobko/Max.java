c927fad9ef2b0eb453e7fb35993d00968d5b01a9
Evgeny <vorokhobko2011@yandex.ru>
2016-12-07 13:57:00 +0300

    add class Max

---
+++ chapter_001/src/main/java/by/vorokhobko/Max.java
@@ -0,0 +1,26 @@
+package by.vorokhobko;
+/**
+ * Max.
+ *
+ * Class Max returns the max number part 001, lesson 3.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 06.12.2016.
+ * @version 1.
+ */
+public class Max {
+	/**
+	* The class field.
+	* Сreating max number.
+	*/
+	private int largeNum;
+	/**
+	* The method determines the max number.
+	* @param first - first.
+	* @param second - second.
+	* @return tag.
+	*/
+	public int max(int first, int second) {
+	largeNum = first < second ? second : first;
+	return largeNum;
+	}
+}
