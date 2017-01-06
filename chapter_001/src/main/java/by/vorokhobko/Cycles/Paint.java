63bbaac958c7f6970c5ba31545e39eb23e0fe00d
Evgeny <vorokhobko2011@yandex.ru>
2017-01-06 13:48:55 +0300

    add

---
+++ chapter_001/src/main/java/by/vorokhobko/Cycles/Paint.java
@@ -0,0 +1,32 @@
+package by.vorokhobko.Cycles;
+
+/**
+* Paint.
+* Class Paint draws a pyramid in character graphics of part 001, lessson 4.
+* @author Vorokhobko Evgeny (vorokhobko2011@yandex.ru).
+* @since 15.12.2016.
+* @version 4
+*/
+
+public class Paint {
+	/**
+	* The class field.
+	* The method draws a pyramid.
+	* @param h - h.
+	* @return tag.
+	*/
+
+	public String pyramid(int h) {
+		StringBuilder sb = new StringBuilder();
+		for (int first = 0; first < h; first++) {
+			for (int second = 0; second <= (h - first); second++) {
+				sb.append(" ");
+			}
+			for (int three = 0; three <= first; three++) {
+				sb.append("^ ");
+			}
+			sb.append(System.getProperty("line.separator"));
+		}
+		return sb.toString();
+	}
+}
---
+++ chapter_001/src/test/java/by/vorokhobko/Conditional_operator/TriangleTest.java
@@ -0,0 +1,67 @@
+package by.vorokhobko.Conditional_operator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import org.junit.Test;
+
+/**
+ * TriangleTest.
+ *
+ * Class TriangleTest for testing class Triangle.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 05.12.2016.
+ * @version 1.
+ */
+
+public class TriangleTest {
+	  /**
+	  * @Test.
+	  * Triangle test positive numbers.
+	  */
+	  @Test
+	public void determineTheAreaOfTheTrianglePositiveNumbers() {
+		final double x = 2;
+		final double y = 5;
+		final double totalArea = 30.00;
+		final double numberExeption = 0.01;
+		final Point pointA = new Point(x, y);
+		final Point pointB = new Point(x + y, y + y);
+		final Point pointC = new Point(x * y, y * y);
+		Triangle triangleABC = new Triangle(pointA, pointB, pointC);
+		double resultArea = triangleABC.area();
+		assertThat(resultArea, is(closeTo(totalArea, numberExeption)));
+		}
+	  /**
+	  * @Test.
+	  * Triangle test negative numbers.
+	  */
+	  @Test
+	public void determineTheAreaOfTheTriangleNegativeNumbers() {
+		final double x = -2;
+		final double y = -5;
+		final double totalArea = 45.00;
+		final double numberExeption = 0.01;
+		final Point pointA = new Point(x, y);
+		final Point pointB = new Point(x + y, y + y);
+		final Point pointC = new Point(x * y, y * y);
+		Triangle triangleABC = new Triangle(pointA, pointB, pointC);
+		double resultArea = triangleABC.area();
+		assertThat(resultArea, is(closeTo(totalArea, numberExeption)));
+		}
+	  /**
+	  * @Test.
+	  * Triangle exeption test.
+	  */
+		@Test(expected = Exception.class)
+	public void determineTheAreaOfTheTriangleExeption() {
+		final double x = 0.5;
+		final double y = 1;
+		final double z = 10;
+		final Point pointA = new Point(x, y);
+		final Point pointB = new Point(y, y);
+		final Point pointC = new Point(z, y);
+		Triangle triangleABC = new Triangle(pointA, pointB, pointC);
+		double resultArea = triangleABC.area();
+		}
+	}
