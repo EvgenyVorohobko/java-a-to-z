de2e8fe20c2bda700dcff6a41e22fa69064c5eda
Evgeny <vorokhobko2011@yandex.ru>
2017-01-06 12:35:33 +0300

    add class Factorial

---
+++ chapter_001/src/main/java/by/vorokhobko/4_Cycles/Factorial.java
@@ -0,0 +1,31 @@
+package by.vorokhobko.4_Cycles;
+
+/**
+* Facorial.
+*
+* Class Factorial calculates the factorial of  part 001, lesson 4.
+* @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+* @since 10.12.2016.
+* @version 1.
+*/
+
+public class Factorial {
+	/**
+	* The class field.
+	* The method calculates the factorial.
+	* @param n - n.
+	* @return tag.
+	*/
+
+	public int mult(int n) {
+		int fact = 1;
+		for (int i = 1; i < n; i++) {
+			fact *= i;
+		}
+		if (n == 0) {
+			int i = 0;
+			fact = 1;
+		}
+		return fact;
+	}
+}
