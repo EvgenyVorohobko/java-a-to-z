c66cc6458f7160d36a04d4e3187ed01bbd5e5c9f
Evgeny <vorokhobko2011@yandex.ru>
2017-01-06 12:59:57 +0300

    Test class Factorial

---
+++ chapter_001/src/test/java/by/vorokhobko/4_Cycles/FactorialTest.java
@@ -0,0 +1,41 @@
+package by.vorokhobko.4_Cycles;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* FacorialTest.
+*
+* Class FactorialTest for testing class Factorial part 001, lesson 4.
+* @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+* @since 10.12.2016.
+* @version 1.
+*/
+
+public class FactorialTest {
+	/**
+	* @Test.
+	* The method calculates the factorial.
+	*/
+	@Test
+	public void computedFactorial() {
+		Factorial fact = new Factorial();
+		final int n = 6;
+		final int totalFact = 120;
+		int result = fact.mult(n);
+		assertThat(result, is(totalFact));
+	}
+	/**
+	* @Test.
+	* The method calculates the factorial if n anyway 0.
+	*/
+	@Test
+	public void computedFactorialZero() {
+		Factorial fact = new Factorial();
+		final int n = 0;
+		final int totalFact = 1;
+		int result = fact.mult(n);
+		assertThat(result, is(totalFact));
+	}
+}
