a31eb6fbfa9cd5ba601c5191b1c704c059b5196b
Evgeny <vorokhobko2011@yandex.ru>
2017-01-24 06:09:07 +0300

    add class Doctor

---
+++ chapter_002/src/main/java/by/vorokhobko/Inheritance/Doctor.java
@@ -0,0 +1,25 @@
+package by.vorokhobko.Inheritance;
+
+/**
+ * Doctor.
+ *
+ * Class Doctor child class that inherits from Profession part 002, lesson 3.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 21.01.2017.
+ * @version 1.
+ */
+public class Doctor extends Profession {
+    /**
+     * The class field.
+     * Class inheritance Profession and the creation of an original metodov and fields.
+     * Сreating internship.
+     * Сreating residency.
+     */
+    private int internship;
+    private int residency;
+    public Doctor(String name, int age, String university, String recommendations, int aScore) {
+        super(name, age, university, recommendations, aScore);
+        this.internship = internship;
+        this.residency = residency;
+    }
+}
