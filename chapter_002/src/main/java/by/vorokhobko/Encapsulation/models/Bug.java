ff756bdda746d36b5d2a7ae412f0eabbde4cd60a
Evgeny <vorokhobko2011@yandex.ru>
2017-02-25 19:38:41 +0300

    add class Bug

---
+++ chapter_002/src/main/java/by/vorokhobko/Encapsulation/models/Bug.java
@@ -0,0 +1,13 @@
+package by.vorokhobko.Encapsulation.models;
+
+/**
+ * BUG.
+ *
+ * Class BUG looking for and error correction bugs in the application part 002, lesson 3.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 24.02.2017.
+ * @version 1.
+ */
+
+public class Bug extends Item {
+}
