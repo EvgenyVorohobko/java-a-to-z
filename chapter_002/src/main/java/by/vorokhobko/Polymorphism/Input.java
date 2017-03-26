bc3ed37c44554d64870d2d00b7a19f01123822ca
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-03-26 21:02:02 +0300

    add interface Input

---
+++ chapter_002/src/main/java/by/vorokhobko/Polymorphism/Input.java
@@ -0,0 +1,19 @@
+package by.vorokhobko.start;
+
+/**
+ * Input.
+ *
+ * Class Input the interface for the program part 002, lesson 5.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 20.03.2017.
+ * @version 1.
+ */
+
+public interface Input {
+	/**
+	* Сreate the class ask.
+	* @param question - question.
+	* @return tag.
+	*/
+	String ask(String question);
+}
