7c3e3322c188bde70b9fd076655e2c224ae244b9
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-03-29 05:56:29 +0300

    add class BugTest

---
+++ chapter_002/src/test/java/by/vorokhobko/Encapsulation/BugTest.java
@@ -0,0 +1,27 @@
+package by.vorokhobko.Encapsulation;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+import by.vorokhobko.Encapsulation.models.Bug;
+import org.junit.Test;
+
+/**
+ * Test.
+ *
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @version 1.
+ * @since 23.02.2017.
+ */
+public class BugTest {
+    /**
+     * Test.
+     */
+    @Test
+    public void whenInItemHaveBug() {
+        Bug bug = new Bug("testName", "testDesc", 5L);
+        String name = "nameTest2";
+        bug.setName(name);
+        String result = bug.getName();
+        assertThat(result, is(name));
+    }
+}
