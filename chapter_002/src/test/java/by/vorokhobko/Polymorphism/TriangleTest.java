4988fd0a052efe008396e9fb2ff31c1bafcc0c80
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-04-04 06:48:20 +0300

    add Test class Triangle

---
+++ chapter_002/src/test/java/by/vorokhobko/Polymorphism/TriangleTest.java
@@ -0,0 +1,29 @@
+package by.vorokhobko.Polymorphism;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * TriangleTest.
+ *
+ * Class TriangleTest for testing class Triangle part 005, lesson 2.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 04.04.2017.
+ * @version 1.
+ */
+
+public class TriangleTest {
+    /**
+     * Test.
+     * The method draws a triangle in pseudographics.
+     */
+    @Test
+    public void drawingOfTheTriangle() {
+        final int h = 3;
+        final Shape triangle = new Triangle(h);
+        String outPrint = "  ^ " + System.getProperty("line.separator") + "  ^ ^ " + System.getProperty("line.separator") + "  ^ ^ ^ " + System.getProperty("line.separator");
+        String result = triangle.pic(triangle);
+        assertThat(result, is(outPrint));
+    }
+}
