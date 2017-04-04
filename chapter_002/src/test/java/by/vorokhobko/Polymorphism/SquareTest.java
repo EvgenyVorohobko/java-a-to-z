8ea03a8217abd82a54b746e9d18867a4f117901b
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-04-04 06:47:52 +0300

    add Test class Square

---
+++ chapter_002/src/test/java/by/vorokhobko/Polymorphism/SquareTest.java
@@ -0,0 +1,34 @@
+package by.vorokhobko.Polymorphism;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * SquareTest.
+ *
+ * Class SquareTest for testing class Square part 005, lesson 2.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 04.04.2017.
+ * @version 1.
+ */
+
+public class SquareTest {
+    /**
+     * Test.
+     * The method draws a square in pseudographics.
+     */
+    @Test
+    public void drawingOfTheTriangle() {
+        final int h = 5;
+        final Shape square = new Square(h);
+        String outPrint = "*****"
+                + System.getProperty("line.separator") + "*   *"
+                + System.getProperty("line.separator") + "*   *"
+                + System.getProperty("line.separator") + "*   *"
+                + System.getProperty("line.separator") + "*****"
+                + System.getProperty("line.separator");
+        String result = square.pic(square);
+        assertThat(result, is(outPrint));
+    }
+}
