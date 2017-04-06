28721079899c9d9949407d762adce0ba19275324
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-04-06 06:24:49 +0300

    add test class Paint

---
+++ chapter_002/src/test/java/by/vorokhobko/Polymorphism/PaintTest.java
@@ -0,0 +1,58 @@
+package by.vorokhobko.Polymorphism;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * PaintTest.
+ *
+ * Class PaintTest for testing class Paint part 005, lesson 2.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 04.04.2017.
+ * @version 1.
+ */
+public class PaintTest {
+    /**
+     * Test.
+     * The method paint figure.
+     */
+    @Test
+    public void whenDrawFiguresTriangle() {
+        final int h = 3;
+        Shape triangle = new Triangle(h);
+        Paint paint = new Paint(triangle);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        paint.draw(triangle);
+        final String result = out.toString();
+        String outPrint = "  ^ "
+                + System.getProperty("line.separator") + "  ^ ^ "
+                + System.getProperty("line.separator") + "  ^ ^ ^ "
+                + System.getProperty("line.separator");
+        assertThat(result, is(outPrint));
+    }
+    /**
+     * Test.
+     * The method paint figure.
+     */
+    @Test
+    public void whenDrawFiguresSquare() {
+        final int h = 5;
+        Shape square = new Square(h);
+        Paint paint = new Paint(square);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        paint.draw(square);
+        final String result = out.toString();
+        String outPrint = "*****"
+                + System.getProperty("line.separator") + "*   *"
+                + System.getProperty("line.separator") + "*   *"
+                + System.getProperty("line.separator") + "*   *"
+                + System.getProperty("line.separator") + "*****"
+                + System.getProperty("line.separator");
+        assertThat(result, is(outPrint));
+    }
+}
