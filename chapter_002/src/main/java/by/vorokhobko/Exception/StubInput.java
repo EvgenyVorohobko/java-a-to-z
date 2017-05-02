0285b8787a7ca0219efcab8a413fc04ad5d07eaa
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-05-02 22:23:23 +0300

    mod class StubInput

--- chapter_002/src/main/java/by/vorokhobko/Exception/StubInput.java
+++ chapter_002/src/main/java/by/vorokhobko/Exception/StubInput.java
@@ -47,7 +47,19 @@ public class StubInput implements Input {
 	 * @param range - range.
 	 * @return tag.
 	 */
-		public int ask(String question, int[] range) {
-			return Integer.valueOf(ask(question));
+	public int ask(String question, int[] range) {
+		int key = Integer.valueOf(this.ask(question));
+		boolean exist = false;
+		for (int value : range) {
+			if (value == key) {
+				exist = true;
+				break;
+			}
 		}
+		if (exist) {
+			return key;
+		} else {
+			throw new MenuOutException("Out of menu range.");
+		}
+	}
 }
