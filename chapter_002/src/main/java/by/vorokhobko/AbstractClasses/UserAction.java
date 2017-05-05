22eca40feada141d7bf4c0f9d13297b5ff848a20
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-05-05 22:29:59 +0300

    add the old classesЭ

---
+++ chapter_002/src/main/java/by/vorokhobko/AbstractClasses/UserAction.java
@@ -0,0 +1,31 @@
+package by.vorokhobko.AbstractClasses;
+
+import by.vorokhobko.Encapsulation.start.Tracker;
+
+/**
+ * UserAction.
+ *
+ * Class UserAction the interface for the program part 002, lesson 6.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 10.04.2017.
+ * @version 1.
+ */
+
+public interface UserAction {
+    /**
+     * Сreate the method key.
+     * @return tag.
+     */
+    int key();
+    /**
+     * Сreate the method execute.
+     * @param input - input.
+     * @param tracker - tracker.
+     */
+    void execute(Input input, Tracker tracker);
+    /**
+     * Сreate the method info.
+     * @return tag.
+     */
+    String info();
+}
