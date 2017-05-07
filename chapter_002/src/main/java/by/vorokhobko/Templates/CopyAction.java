8539ac536d0faaa1595a36afb3cb5b3ec2a40e06
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-05-07 13:12:36 +0300

    mod classes

--- chapter_002/src/main/java/by/vorokhobko/Templates/CopyAction.java
+++ chapter_002/src/main/java/by/vorokhobko/Templates/CopyAction.java
@@ -1,11 +1,21 @@
 package by.vorokhobko.Templates;
- 
- public class CopyAction extends TemplatesAction {
+/**
+ * CopyAction.
+ *
+ * Class CopyAction extends TemplatesAction 002, lesson 7.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 05.05.2017.
+ * @version 1.
+ */
+public class CopyAction extends TemplatesAction {
+	/**
+	 * Method determining the start program.
+	 */
 	 public void start() {
-		 
 	 }
-
+	/**
+	 * Method determining the finish program.
+	 */
 	 public void finish() {
-		 
 	 }
  }
--- chapter_002/src/main/java/by/vorokhobko/Templates/TemplatesAction.java
+++ chapter_002/src/main/java/by/vorokhobko/Templates/TemplatesAction.java
@@ -1,16 +1,33 @@
- package by.vorokhobko.Templates;
- 
- public abstract class TemplatesAction {
-	 abstract void start();
-	 
-	 abstract void finish();
-	 
-	 public void work() {
-		 this.start();
-		 this.finish();
-	 }
-	 
-	 public static void main(String[] args) {
-		 new CopyAction();
-	 }
- }
+package by.vorokhobko.Templates;
+/**
+ * TemplatesAction.
+ *
+ * Class TemplatesAction abstract class 002, lesson 7.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 05.05.2017.
+ * @version 1.
+ */
+public abstract class TemplatesAction {
+	/**
+	 * Abstract method determining the start program.
+	 */
+	abstract void start();
+	/**
+	 * Abstract method determining the finish program.
+	 */
+	abstract void finish();
+	/**
+	 * Method determining work program.
+	 */
+	public void work() {
+		this.start();
+		this.finish();
+	}
+	/**
+	 * Main method.
+	 * @param args - args.
+	 */
+	public static void main(String[] args) {
+		new CopyAction();
+	}
+}
