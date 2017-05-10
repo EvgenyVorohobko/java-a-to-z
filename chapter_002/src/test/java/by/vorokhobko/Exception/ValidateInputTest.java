685630d1a1cc792ff4b2824571906cae499f74b7
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-05-10 20:58:55 +0300

    add classes ConsoleInputTest

---
+++ chapter_002/src/test/java/by/vorokhobko/Exception/ValidateInputTest.java
@@ -0,0 +1,7 @@
+package by.vorokhobko.Exception;
+
+/**
+ * Created by Windows10x64 on 07.05.2017.
+ */
+public class ValidateInputTest {
+}
---
+++ chapter_002/src/test/java/by/vorokhobko/InnerClasses/ConsoleInputTest.java
@@ -0,0 +1,7 @@
+package by.vorokhobko.InnerClasses;
+
+/**
+ * Created by Windows10x64 on 10.05.2017.
+ */
+public class ConsoleInputTest {
+}
