30ab03d3e0b793cd8dc5c76e91e607a216b2a35e
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-05-14 14:52:46 +0300

    add class Type

---
+++ chapter_002/src/main/java/by/vorokhobko/Chess/models/Type.java
@@ -0,0 +1,13 @@
+package by.vorokhobko.Chess.models;
+
+/**
+ * Type.
+ *
+ * Class Type a listing of all figures in Chess 002, lesson test.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 14.05.2017.
+ * @version 1.
+ */
+public enum Type {
+    BISHOP, KING, QUEEN, ROOK, PAWN, KNIGHT
+}
