3ddb65ff96b6ed86c1a677438fe48e1801533ffd
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-05-14 21:26:46 +0300

    mod class Figure

--- chapter_002/src/main/java/by/vorokhobko/Chess/models/Figure.java
+++ chapter_002/src/main/java/by/vorokhobko/Chess/models/Figure.java
@@ -14,6 +14,22 @@ public abstract class Figure {
     /**
      * The class field.
      */
+    private int finishX;
+    /**
+     * The class field.
+     */
+    private int finishY;
+    /**
+     * The class field.
+     */
+    private int lengthArray;
+    /**
+     * The class field.
+     */
+    private Cell[] pass;
+    /**
+     * The class field.
+     */
     private final Cell position;
     /**
      * Сreate the class constructor Figure.
@@ -36,6 +52,54 @@ public abstract class Figure {
      * @return tag.
      */
     public abstract Cell[] way(Cell dist) throws ImposibleMoveException;
+    /**
+     * Method determining can move figure.
+     * @param distance - distance.
+     * @throws ImposibleMoveException tag.
+     * @return tag.
+     */
+    public Cell[] moveFigure(Cell distance) throws ImposibleMoveException {
+        int startX = position.getX();
+        int startY = position.getY();
+        finishX = distance.getX();
+        finishY = distance.getY();
+
+        int distanceX = Math.abs(this.finishX - startX);
+        int distanceY = Math.abs(this.finishY - startY);
+
+        if (((finishX - startX) > 0) & ((finishY - startY) > 0)) {
+            pass = new Cell[lengthArray];
+            for (int i = 0; i < lengthArray; i++){
+                startX++;
+                startY++;
+                pass[i] = new Cell(startX, startY);
+            }
+        } else if (((finishX - startX) < 0) & ((finishY - startY) < 0)) {
+            pass = new Cell[lengthArray];
+            for (int i = 0; i < lengthArray; i++) {
+                startX--;
+                startY--;
+                pass[i] = new Cell(startX, startY);
+            }
+        } else if (((finishX - startX) > 0) & ((finishY - startY) < 0)) {
+            pass = new Cell[lengthArray];
+            for (int i = 0; i < lengthArray; i++) {
+                startX++;
+                startY--;
+                pass[i] = new Cell(startX, startY);
+            }
+        } else if (((finishX - startX) < 0) & ((finishY - startY) > 0)) {
+            pass = new Cell[lengthArray];
+            for (int i = 0; i < lengthArray; i++) {
+                startX--;
+                startY++;
+                pass[i] = new Cell(startX, startY);
+            }
+        } else {
+            throw new ImposibleMoveException("Figure do not move that.");
+        }
+        return pass;
+    }
     /**
      * Abstract method determining type figure.
      * @return tag.
