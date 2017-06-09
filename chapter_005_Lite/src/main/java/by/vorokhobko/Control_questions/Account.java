375a12760ce6ba069d532479fe8f79ab53fadfea
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-06-09 14:44:08 +0300

    add classes fo tests

---
+++ chapter_005_Lite/src/main/java/by/vorokhobko/Control_questions/Account.java
@@ -0,0 +1,40 @@
+package by.vorokhobko.Control_questions;
+
+public class Account {
+    /**
+     * The class field.
+     */
+    private int value;
+    /**
+     * The class field.
+     */
+    private int requisites;
+    /**
+     * Add getter requisites.
+     * @return tag.
+     */
+    public int getRequisites() {
+        return requisites;
+    }
+    /**
+     * Add setter requisites.
+     * @param requisites - requisites.
+     */
+    public void setRequisites(int requisites) {
+        this.requisites = requisites;
+    }
+    /**
+     * Add getter value.
+     * @return tag.
+     */
+    public int getValue() {
+        return value;
+    }
+    /**
+     * Add setter value.
+     * @param value - value.
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
---
+++ chapter_005_Lite/src/main/java/by/vorokhobko/Control_questions/User.java
@@ -0,0 +1,69 @@
+package by.vorokhobko.Control_questions;
+
+/**
+ * User.
+ *
+ * Class User for control questions 005_Lite, lesson 4.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 09.06.2017.
+ * @version 1.
+ */
+public class User {
+    /**
+     * The class field.
+     */
+    private String name;
+    /**
+     * The class field.
+     */
+    private int passport;
+    /**
+     * Add constructor.
+     * @param passport - passport.
+     * @param name - name.
+     */
+    public User(String name, int passport) {
+        this.name = name;
+        this.passport = passport;
+    }
+    /**
+     * Override method equals.
+     * @param o - o.
+     * @return tag.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (passport != user.passport) return false;
+        return !(name != null ? !name.equals(user.name) : user.name != null);
+
+    }
+    /**
+     * Override method hashCode.
+     * @return tag.
+     */
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + passport;
+        return result;
+    }
+    /**
+     * Add getter name.
+     * @return tag.
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * Add getter passport.
+     * @return tag.
+     */
+    public int getPassport() {
+        return passport;
+    }
+}
