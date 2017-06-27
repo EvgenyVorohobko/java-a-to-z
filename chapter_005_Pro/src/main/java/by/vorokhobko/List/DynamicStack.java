0425f80b413ecf258ba99f099793b592819ce71c
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-06-28 01:01:10 +0300

    add Stack

---
+++ chapter_005_Pro/src/main/java/by/vorokhobko/List/DynamicStack.java
@@ -0,0 +1,68 @@
+package by.vorokhobko.List;
+
+import java.util.Iterator;
+
+/**
+ * DynamicStack.
+ *
+ * Class DynamicStack create the Stack for 005_Pro, lesson 3.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 27.06.2017.
+ * @version 1.
+ * @param <E>.
+ */
+public class DynamicStack<E> {
+    /**
+     * The class field.
+     */
+    private DynamicLinkedList<E> linked = new DynamicLinkedList<>();
+    /**
+     * The class field.
+     */
+    private Iterator<E> iter = linked.iterator();
+    /**
+     * The class field.
+     */
+    private int count;
+    /**
+     * Add objects in Stack.
+     * @param value - value.
+     */
+    public void push(E value) {
+        linked.add(value);
+    }
+    /**
+     * Get top position in Stack and remove that.
+     * @throws InvalidOperationException tag.
+     * @return tag.
+     */
+    public E pop() throws InvalidOperationException {
+        if (getCount() != 0) {
+            E result = linked.get(linked.size() - 1);
+            linked.remove();
+            return result;
+        } else {
+            throw new InvalidOperationException("The stack is empty");
+        }
+    }
+    /**
+     * Get top position in Stack.
+     * @throws InvalidOperationException tag.
+     * @return tag.
+     */
+    public E peek() throws InvalidOperationException {
+        if (getCount() != 0) {
+            E result = linked.get(linked.size() - 1);
+            return result;
+        } else {
+            throw new InvalidOperationException("The stack is empty");
+        }
+    }
+    /**
+     * Get positions in Stack.
+     * @return tag.
+     */
+    public int getCount() {
+        return linked.size();
+    }
+}
--- chapter_005_Pro/src/test/java/by/vorokhobko/List/DynamicStackTest.java
+++ chapter_005_Pro/src/test/java/by/vorokhobko/List/DynamicStackTest.java
@@ -1,7 +1,49 @@
 package by.vorokhobko.List;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+import java.util.Iterator;
+
 /**
- * Created by Windows10x64 on 27.06.2017.
+ * Test.
+ *
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @version 1.
+ * @since 25.06.2017.
  */
 public class DynamicStackTest {
+    /**
+     * The class field.
+     */
+    private DynamicStack<Integer> stack = new DynamicStack<>();
+    /**
+     * Test.
+     */
+    @Test
+    public void whenAddElementInStack() throws InvalidOperationException  {
+        stack.push(4);
+        stack.push(3);
+        assertThat(stack.peek(), is(3));
+        assertThat(stack.getCount(), is(2));
+    }
+    /**
+     * Test by Exception.
+     * @throws InvalidOperationException tag.
+     */
+    @Test(expected = InvalidOperationException.class)
+    public void whenNotElementInStack() throws InvalidOperationException {
+        stack.peek();
+    }
+    /**
+     * Test.
+     */
+    @Test
+    public void whenDeleteElementInStack() throws InvalidOperationException  {
+        stack.push(4);
+        stack.push(3);
+        stack.pop();
+        assertThat(stack.peek(), is(4));
+    }
 }
