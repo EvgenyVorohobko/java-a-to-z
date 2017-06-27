38371cf472cab78580e85ebc46d514e01fd04899
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-06-28 01:27:34 +0300

    add Queue and test

---
+++ chapter_005_Pro/src/main/java/by/vorokhobko/List/DynamicQueue.java
@@ -0,0 +1,58 @@
+package by.vorokhobko.List;
+
+/**
+ * DynamicQueue.
+ *
+ * Class DynamicQueue create the Queue for 005_Pro, lesson 3.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 28.06.2017.
+ * @version 1.
+ * @param <E>.
+ */
+public class DynamicQueue<E> {
+    /**
+     * The class field.
+     */
+    private DynamicLinkedList<E> linked = new DynamicLinkedList<>();
+    /**
+     * Add objects in Queue.
+     * @param value - value.
+     */
+    public void enqueue(E value) {
+        linked.add(value);
+    }
+    /**
+     * Get top position in Queue and remove that.
+     * @throws InvalidOperationException tag.
+     * @return tag.
+     */
+    public E dequeue() throws InvalidOperationException {
+        if (getCount() != 0) {
+            E result = linked.get(1);
+            linked.remove();
+            return result;
+        } else {
+            throw new InvalidOperationException("The stack is empty!");
+        }
+    }
+    /**
+     * Get top position in Queue.
+     * @throws InvalidOperationException tag.
+     * @return tag.
+     */
+    public E peek() throws InvalidOperationException {
+        if (getCount() != 0) {
+            E result = linked.get(linked.size() - 1);
+            return result;
+        } else {
+            throw new InvalidOperationException("The stack is empty!");
+        }
+    }
+    /**
+     * Get positions in Stack.
+     * @return tag.
+     */
+    public int getCount() {
+        return linked.size();
+    }
+}
---
+++ chapter_005_Pro/src/test/java/by/vorokhobko/List/DynamicQueueTest.java
@@ -0,0 +1,49 @@
+package by.vorokhobko.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+/**
+ * Test.
+ *
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @version 1.
+ * @since 28.06.2017.
+ */
+public class DynamicQueueTest {
+    /**
+     * The class field.
+     */
+    private DynamicQueue<Integer> queue = new DynamicQueue<>();
+    /**
+     * Test.
+     * @throws InvalidOperationException tag.
+     */
+    @Test
+    public void whenAddElementInStack() throws InvalidOperationException  {
+        queue.enqueue(4);
+        queue.enqueue(3);
+        assertThat(queue.peek(), is(3));
+        assertThat(queue.getCount(), is(2));
+    }
+    /**
+     * Test by Exception.
+     * @throws InvalidOperationException tag.
+     */
+    @Test(expected = InvalidOperationException.class)
+    public void whenNotElementInStack() throws InvalidOperationException {
+        queue.peek();
+    }
+    /**
+     * Test.
+     * @throws InvalidOperationException tag.
+     */
+    @Test
+    public void whenDeleteElementInStack() throws InvalidOperationException  {
+        queue.enqueue(4);
+        queue.enqueue(3);
+        queue.dequeue();
+        assertThat(queue.getCount(), is(1));
+    }
+}
