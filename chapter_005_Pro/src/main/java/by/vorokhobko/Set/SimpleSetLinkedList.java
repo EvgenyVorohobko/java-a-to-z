24eef8d383afbde51c3701fca73e8ea37008240a
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-07-07 19:36:37 +0300

    update 2. Set реализованный на связном списке. [#997]

--- chapter_005_Pro/src/main/java/by/vorokhobko/Set/SimpleSetLinkedList.java
+++ chapter_005_Pro/src/main/java/by/vorokhobko/Set/SimpleSetLinkedList.java
@@ -1,6 +1,9 @@
 package by.vorokhobko.Set;
 
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
@@ -16,168 +19,51 @@ public class SimpleSetLinkedList<E> implements Iterable {
     /**
      * The class field.
      */
-    private Node<E> firstNode;
-    /**
-     * The class field.
-     */
-    private Node<E> lastNode;
-    /**
-     * The class field.
-     */
-    private int size;
-    /**
-     * The class field.
-     */
-    private int counter = 0;
+    private List<E> list;
     /**
      * Add constructor.
      */
     public SimpleSetLinkedList() {
-        this.lastNode = new Node<E>(firstNode, null, null);
-        this.firstNode = new Node<E>(null, null, lastNode);
+        this.list = new LinkedList<>();
     }
     /**
-     * Node.
-     *
-     * Class Node for start SimpleSetLinkedList 005_Pro, lesson 4.
-     * @param <E>.
+     * The class field.
      */
-    private class Node<E> {
-        /**
-         * The class field.
-         */
-        private E currentElement;
-        /**
-         * The class field.
-         */
-        private Node<E> nextElement;
-        /**
-         * The class field.
-         */
-        private Node<E> prevElement;
-        /**
-         * Add constructor.
-         * @param currentElement - currentElement.
-         * @param nextElement    - nextElement.
-         * @param prevElement    - prevElement.
-         */
-        private Node(Node<E> prevElement, E currentElement, Node<E> nextElement) {
-            this.currentElement = currentElement;
-            this.nextElement = nextElement;
-            this.prevElement = prevElement;
-        }
-        /**
-         * Add getter currentElement.
-         * @return tag.
-         */
-        public E getCurrentElement() {
-            return currentElement;
-        }
-        /**
-         * Add setter currentElement.
-         * @param currentElement - currentElement.
-         */
-        public void setCurrentElement(E currentElement) {
-            this.currentElement = currentElement;
-        }
-        /**
-         * Add getter currentElement.
-         * @return tag.
-         */
-        public Node<E> getNextElement() {
-            return nextElement;
-        }
-        /**
-         * Add setter nextElement.
-         * @param nextElement - nextElement.
-         */
-        public void setNextElement(Node<E> nextElement) {
-            this.nextElement = nextElement;
-        }
-    }
+    private int count = 0;
     /**
      * Add objects in SimpleSetLinkedList.
      * @param newElement - newElement.
      */
-    public void add(E newElement) {
-        Node<E> previous = this.lastNode;
-        previous.setCurrentElement(newElement);
-        lastNode = new Node<E>(previous, null, null);
-        previous.setNextElement(lastNode);
-        size++;
-        for (int i = 0; i < size; i++) {
-            for (int j = i + 1; j < size; j++) {
-                if (iterator().hasNext()) {
-                    if ((iterator().next().equals(newElement))) {
-                        System.out.println("This object is already in the array!");
-                        remove();
-                        break;
-                    }
+    public void addList(E newElement) {
+        if (list.size() > 1) {
+            for (int i = 0; i < list.size(); i++) {
+                if ((getElement(i).equals(newElement))) {
+                    System.out.println("This element have in list!");
                 }
             }
+            list.add(newElement);
+        } else {
+            list.add(newElement);
         }
+        sortedList();
+    }
+    /**
+     * Method sort element in list by hashCode.
+     */
+    public List<E> sortedList() {
+        Collections.sort(list, (elementOne, elementTwo) -> Integer.compare(elementOne.hashCode(), elementTwo.hashCode()));
+        return list;
     }
     /**
      * Get object in SimpleSetLinkedList.
      * @param index - index.
      * @return tag.
      */
-    public E get(int index) {
-        if (index < size) {
-            Node<E> target = firstNode.getNextElement();
-            for (int i = 0; i < index; i++) {
-                target = getNextElement(target);
-            }
-            return target.getCurrentElement();
-        } else {
-            throw new NoSuchElementException("The objects in the array is no more!");
-        }
+    public E getElement(int index) {
+        return list.get(index);
     }
-    /**
-     * Get help in get object.
-     * @param current - current.
-     * @return tag.
-     */
-    private Node<E> getNextElement(Node<E> current) {
-        return current.getNextElement();
-    }
-    /**
-     * Get size.
-     * @return tag.
-     */
     public int size() {
-        return size;
-    }
-    /**
-     * Delete element in SimpleSetLinkedList.
-     * @return tag.
-     */
-    public E remove() {
-        Node<E> delete = lastNode;
-        if (delete != null) {
-            return deleteElementInList(delete);
-        } else {
-            throw new NoSuchElementException();
-        }
-    }
-    /**
-     * Help for method remove in SimpleSetLinkedList.
-     * @param delete - delete.
-     * @return tag.
-     */
-    public E deleteElementInList(Node<E> delete) {
-        final E element = delete.currentElement;
-        final Node<E> prev = delete.prevElement;
-        delete.currentElement = null;
-        delete.prevElement = null;
-        lastNode = prev;
-        if (prev == null) {
-            firstNode = null;
-        } else {
-            prev.nextElement = null;
-        }
-        size--;
-        return element;
+        return list.size();
     }
     /**
      * Override iterator constructor.
@@ -200,7 +86,11 @@ public class SimpleSetLinkedList<E> implements Iterable {
          */
         @Override
         public boolean hasNext() {
-            return size > counter;
+            boolean isNeedSave = false;
+            if (list.size() > count) {
+                isNeedSave = true;
+            }
+            return isNeedSave;
         }
         /**
          * Override method next.
@@ -208,7 +98,11 @@ public class SimpleSetLinkedList<E> implements Iterable {
          */
         @Override
         public Object next() {
-            return get(counter++);
+            if (hasNext()) {
+                return list.get(count++);
+            } else {
+                throw new NoSuchElementException();
+            }
         }
     }
 }
