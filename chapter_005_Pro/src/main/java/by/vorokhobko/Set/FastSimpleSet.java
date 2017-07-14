5e3daf3bb7982f879f7984463eebf036b0ac6559
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-07-14 13:55:52 +0300

    3. Увеличение скорости вставки объекта в коллекцию типа Set [#998]

---
+++ chapter_005_Pro/src/main/java/by/vorokhobko/Set/FastSimpleSet.java
@@ -0,0 +1,82 @@
+package by.vorokhobko.Set;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * FastSimpleSet.
+ *
+ * Class FastSimpleSet create fast add element in Set 005_Pro, lesson 4.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 14.07.2017.
+ * @version 1.
+ * @param <E>.
+ */
+public class FastSimpleSet<E> {
+    /**
+     * The class field.
+     */
+    private List<E> list;
+    /**
+     * Add constructor.
+     */
+    public FastSimpleSet() {
+        this.list = new ArrayList<>();
+    }
+    /**
+     * Add objects in Set.
+     * @param element - element.
+     */
+    public void add(E element) {
+        determinesPositionElement(element, this.list, 0, list.size());
+    }
+    /**
+     * Get object.
+     * @param index - index.
+     * @return tag.
+     */
+    public E get(int index) {
+        return list.get(index);
+    }
+    /**
+     * Get size.
+     * @return tag.
+     */
+    public int size() {
+        return list.size();
+    }
+    /**
+     * Method determines position elements in Set.
+     * @param element - element.
+     * @param newList - newList.
+     * @param start - start.
+     * @param finish - finish.
+     */
+    private void determinesPositionElement(E element, List<E> newList, int start, int finish) {
+        int middle = start - (start - finish) / 2;
+        if (start == finish) {
+            list.add(element);
+        }
+        if (start < finish) {
+            if (element.hashCode() < newList.get(middle).hashCode()) {
+                addElement(element);
+            } else if (element.hashCode() > newList.get(middle).hashCode()) {
+                addElement(element);
+            }
+        }
+    }
+    /**
+     * Add objects in List.
+     * @param element - element.
+     */
+    private void addElement(E element) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).hashCode() > element.hashCode()) {
+                list.add(i, element);
+                break;
+            } else if (list.get(i).hashCode() == element.hashCode()) {
+                System.out.println("This element " + element + " have in Set!");
+            }
+        }
+    }
+}
---
+++ chapter_005_Pro/src/test/java/by/vorokhobko/Set/FastSimpleSetTest.java
@@ -0,0 +1,54 @@
+package by.vorokhobko.Set;
+
+import org.junit.Test;
+import java.util.Iterator;
+
+/**
+ * Test.
+ *
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @version 1.
+ * @since 14.07.2017.
+ */
+public class FastSimpleSetTest {
+    /**
+     * The class field.
+     */
+    private FastSimpleSet<Integer> set = new FastSimpleSet<>();
+    /**
+     * The class field.
+     */
+    private static final int SIZE = 100;
+    /**
+     * The class field.
+     */
+    private SimpleSetArray<Integer> setArray = new SimpleSetArray<>(SIZE);
+    /**
+     * The class field.
+     */
+    private Iterator<Integer> iter = setArray.iterator();
+    /**
+     * Test.
+     */
+    @Test
+    public void whenAddElementsInArrayTime1() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < SIZE; i++) {
+            set.add(i);
+        }
+        long timeWorkCode = System.currentTimeMillis() - start;
+        System.out.println(timeWorkCode);
+    }
+    /**
+     * Test.
+     */
+    @Test
+    public void whenAddElementsInArrayTime2() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < SIZE; i++) {
+            setArray.add(i);
+        }
+        long timeWorkCode = System.currentTimeMillis() - start;
+        System.out.println(timeWorkCode);
+    }
+}
