bc986b6edc9822a4154bf08f78f1cc4f33475cf5
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-07-16 21:53:21 +0300

    5. Перекрывать и equals и hashCode[#19463]

---
+++ chapter_005_Pro/src/main/java/by/vorokhobko/Map/UserOverrideHashCodeAndEquals.java
@@ -0,0 +1,56 @@
+package by.vorokhobko.Map;
+
+import java.util.Calendar;
+
+/**
+ * UserOverrideHashCodeAndEquals.
+ *
+ * Class UserOverrideHashCodeAndEquals for Map, lesson 5.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 14.07.2017.
+ * @version 1.
+ */
+public class UserOverrideHashCodeAndEquals extends User {
+    /**
+     * Add constructor.
+     * @param name - name.
+     * @param children - children.
+     * @param birthday - birthday.
+     */
+    public UserOverrideHashCodeAndEquals(String name, int children, Calendar birthday) {
+        super(name, children, birthday);
+    }
+    /**
+     * Override method equals.
+     * @param o - o.
+     * @return tag.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserOverrideHashCodeAndEquals that = (UserOverrideHashCodeAndEquals) o;
+        if (getChildren() != that.getChildren()) {
+            return false;
+        }
+        if (getName() != null ? !getName().equals(that.getName()) : that.getName() != null) {
+            return false;
+        }
+        return !(getBirthday() != null ? !getBirthday().equals(that.getBirthday()) : that.getBirthday() != null);
+    }
+    /**
+     * Override method hashCode.
+     * @return tag.
+     */
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + getChildren();
+        result = 31 * result + (getBirthday() != null ? getBirthday().hashCode() : 0);
+        return result;
+    }
+}
