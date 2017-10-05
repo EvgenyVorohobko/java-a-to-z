3b1da491b035e273137412d95a0a3bd336d80f01
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-10-05 22:32:55 +0300

    2. Реализовать ThreadPool

---
+++ chapter_007/src/main/java/by/vorokhobko/Wait_Notify_NotifyAll/Thread_Pool/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * //TODO add comments.
+ *
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @version 1.
+ * @since 11.11.2016.
+ */
+ package by.vorokhobko.Wait_Notify_NotifyAll.Thread_Pool;
