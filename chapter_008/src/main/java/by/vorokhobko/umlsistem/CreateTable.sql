34479595a263483393b2a3581d6a0a347b220929
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-11-05 23:28:20 +0300

    update UML системы

--- chapter_008/src/main/java/by/vorokhobko/umlsistem/CreateTable.sql
+++ chapter_008/src/main/java/by/vorokhobko/umlsistem/CreateTable.sql
@@ -1,18 +1,34 @@
 create database java_a_to_z;
 
+create table enter (
+	enter_id serial PRIMARY KEY,
+	user_login CHARACTER VARYING (30),
+	password CHARACTER VARYING (30)
+)
+
+create table items (
+	item_id PRIMARY KEY,
+	date_id INTEGER REFERENCES attached_files (date_id),
+	application_id INTEGER REFERENCES application (application_id),
+	role_id INTEGER REFERENCES role (role_id),
+	create_data timestamp
+	id_enter INTEGER REFERENCES enter (enter_id),
+)
+
 create table user (
 	user_id serial PRIMARY KEY,
-	user_login CHARACTER VARYING (30), 
+	user_login INTEGER REFERENCES enter (user_login), 
 	name CHARACTER VARYING (15),
 	create_information CHARACTER VARYING (100),
-	password CHARACTER VARYING (30)
+	password INTEGER REFERENCES enter (password),
+	id_item INTEGER REFERENCES items (item_id)
 )
 
 create table role (
 	role_id serial PRIMARY KEY,
 	status_role CHARACTER VARYING (20),
 	description_role CHARACTER VARYING (1000),
-	id_user INTEGER REFERENCES user (user_id)
+	id_item INTEGER REFERENCES items (item_id)
 )
 
 create table rule_role (
@@ -33,19 +49,11 @@ create table application (
 	application_id PRIMARY KEY,
 	applications CHARACTER VARYING (400),
 	description_application CHARACTER VARYING (1000),
-	id_user INTEGER REFERENCES user (user_id)
+	id_item INTEGER REFERENCES items (item_id)
 )
 
 create table comment_application (
 	comment_id PRIMARY KEY,
 	comments CHARACTER VARYING (500),
 	id_application INTEGER REFERENCES application(application_id)
-)
-
-create table items (
-	item_id PRIMARY KEY,
-	date_id INTEGER REFERENCES attached_files (date_id),
-	application_id INTEGER REFERENCES application (application_id),
-	role_id INTEGER REFERENCES role (role_id),
-	create_data timestamp
 )
