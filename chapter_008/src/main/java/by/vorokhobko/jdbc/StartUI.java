e2dfbc854e949d2fea0a21facc8398ca46839059
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2017-12-08 13:27:33 +0300

    Update XML XSLT JDBC Оптимизация

--- chapter_008/src/main/java/by/vorokhobko/jdbc/StartUI.java
+++ chapter_008/src/main/java/by/vorokhobko/jdbc/StartUI.java
@@ -20,11 +20,19 @@ public class StartUI {
     /**
      * The class field.
      */
-    private static WorkSQL workSQL = new WorkSQL();
+    private static final String XML_FILE_FIRST = "d:/WORK/SQLlite/TEST/1.xml";
     /**
      * The class field.
      */
-    private static SAXPars pars = new SAXPars();
+    private static final String XML_FILE_SECOND = "d:/WORK/SQLlite/TEST/2.xml";
+    /**
+     * The class field.
+     */
+    private WorkSQL workSQL = new WorkSQL();
+    /**
+     * The class field.
+     */
+    private SAXPars pars = new SAXPars();
     /**
      * The class field.
      */
@@ -32,34 +40,28 @@ public class StartUI {
     /**
      * The class field.
      */
-    private static long start;
-    /**
-     * The class field.
-     */
-    private static TransformXMLDocument trans = new TransformXMLDocument();
-    /**
-     * The class field.
-     */
-    private static final String XML_FILE_FIRST = "d:/WORK/SQLlite/TEST/1.xml";
-    /**
-     * The class field.
-     */
-    private static final String XML_FILE_SECOND = "d:/WORK/SQLlite/TEST/2.xml";
+    private TransformXMLDocument trans = new TransformXMLDocument();
     /**
      * The method main.
      * @param args - args.
      */
     public static void main(String[] args) {
-        start = System.currentTimeMillis();
-        workSQL.createConnection(URL);
-        workSQL.createTable();
-        workSQL.addElement(ELEMENT);
-        workSQL.createDocumentXML();
-        workSQL.closeConnection();
-        trans.updateXML(XML_FILE_FIRST, XML_FILE_SECOND);
-        long result = pars.parsingXMLDocument(new File(XML_FILE_SECOND));
+        long start = System.currentTimeMillis();
+        new StartUI().runWork();
         long timeWorkCode = System.currentTimeMillis() - start;
-        System.out.println("Sum all element " + result);
         System.out.println("Time work " + timeWorkCode);
+    }
+    /**
+     * The method need for start program.
+     */
+    public void runWork() {
+        this.workSQL.createConnection(URL);
+        this.workSQL.createTable();
+        this.workSQL.addElement(ELEMENT);
+        this.workSQL.createDocumentXML();
+        this.workSQL.closeConnection();
+        this.trans.updateXML(XML_FILE_FIRST, XML_FILE_SECOND);
+        long result = pars.parsingXMLDocument(new File(XML_FILE_SECOND));
+        System.out.println("Sum all element " + result);
     }
 }
