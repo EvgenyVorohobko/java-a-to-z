a662e5772d097c4aaba8c5d57ab74d5e153b2baa
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2018-01-19 20:21:11 +0300

    Crud servlet

---
+++ chapter_004/httpprotocol/src/main/java/by/vorokhobko/http/UserStore.java
@@ -0,0 +1,184 @@
+package by.vorokhobko.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * UserStore.
+ *
+ * Class UserStore is the inner part of the work with the database part 004, lesson 2.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 13.01.2018.
+ * @version 1.
+ */
+public class UserStore {
+    /**
+     * The class field.
+     */
+    private static final String URL = "jdbc:postgresql://localhost:5432";
+    /**
+     * The class field.
+     */
+    private static final String USERNAME = "postgres";
+    /**
+     * The class field.
+     */
+    private static final String PASSWORD = "1";
+    /**
+     * The class field.
+     */
+    private static final String DRIVER_NAME = "org.postgresql.Driver";
+    /**
+     * The class field.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(UserStore.class);
+    /**
+     * The class field.
+     */
+    private Connection connection;
+    /**
+     * The class field.
+     */
+    private static UserStore instance;
+    /**
+     * Add constructor.
+     */
+    private UserStore() {
+    }
+    /**
+     * The method synchronized in class UserStore.
+     * @return tag.
+     */
+    public static synchronized UserStore getInstance() {
+        if (instance == null)
+            instance = new UserStore();
+        return instance;
+    }
+    /**
+     * The method create connection with database.
+     */
+    public void createConnection() {
+        try {
+            Class.forName(DRIVER_NAME);
+            this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            this.connection.setAutoCommit(false);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
+    /**
+     * The method create database.
+     */
+    public void createTable() {
+        try (Statement statement = this.connection.createStatement()) {
+            statement.execute("CREATE TABLE if NOT EXISTS userWork("
+                    + "id INTEGER PRIMARY KEY,"
+                    + "nameUser VARCHAR(100),"
+                    + "loginUser VARCHAR(100),"
+                    + "emailUser VARCHAR(100),"
+                    + "createDate TIMESTAMP)");
+            this.connection.commit();
+            statement.close();
+        } catch (SQLException e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
+    /**
+     * The method adds a user to database.
+     * @param user - user.
+     */
+    public void addUser(User user) {
+        String sqlQuestion = "INSERT INTO userWork(nameUser, loginUser, emailUser, createDate) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement statement = this.connection.prepareStatement(sqlQuestion)) {
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getLogin());
+            statement.setString(3, user.getEmail());
+            statement.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
+            statement.executeUpdate();
+            this.connection.commit();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * The method updates a user to database.
+     * @param user - user.
+     */
+    public void updateUser(User user) {
+        String sqlQuestion = "UPDATE userWork SET nameUser = ?, loginUser = ?, emailUser = ?, createDate = ? WHERE id = ?";
+        try (PreparedStatement statement = this.connection.prepareStatement(sqlQuestion)) {
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getLogin());
+            statement.setString(3, user.getEmail());
+            statement.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
+            statement.executeUpdate();
+            this.connection.commit();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * The method deletes duplicate.
+     */
+    public void removeUser(int id) {
+        String sqlQuestion = "DELETE FROM userWork WHERE id = ?";
+        try (PreparedStatement statement = this.connection.prepareStatement(sqlQuestion)) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+            this.connection.commit();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * The method searches all users.
+     * @return tag.
+     */
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        String sqlQuestion = "SELECT * FROM userWork";
+        try (PreparedStatement statement = this.connection.prepareStatement(sqlQuestion)) {
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                User result = new User(
+                        rs.getString("nameUser"),
+                        rs.getString("loginUser"),
+                        rs.getString("emailUser"),
+                        rs.getTimestamp("createDate")
+                );
+                result.setId(rs.getInt("id"));
+                list.add(result);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    /**
+     * The method close connection with database.
+     */
+    public void closeConnection() {
+        try {
+            this.connection.close();
+            this.connection = null;
+        } catch (SQLException e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            if (this.connection != null) {
+                try {
+                    this.connection.close();
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
+}
---
+++ chapter_004/httpprotocol/src/main/java/by/vorokhobko/http/UsersServlet.java
@@ -0,0 +1,92 @@
+package by.vorokhobko.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+
+/**
+ * UsersServlet.
+ *
+ * Class UsersServlet is the part of the work with web-service part 004, lesson 2.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 13.01.2018.
+ * @version 1.
+ */
+public class UsersServlet extends HttpServlet {
+    /**
+     * The class field.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(HttpServlet.class);
+    /**
+     * The class field.
+     */
+    private UserStore users = UserStore.getInstance();
+    /**
+     * The method starts working with the database.
+     */
+    @Override
+    public void init() throws ServletException {
+        this.users.createConnection();
+        this.users.createTable();
+    }
+    /**
+     * The method handle a GET request.
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        for (User user : users.findAll()) {
+            writer.append(user.toString());
+            writer.append(System.getProperty("line.separator"));
+            writer.flush();
+        }
+    }
+    /**
+     * The method handle a Post request.
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        this.users.addUser(new User(
+                req.getParameter("nameUser"),
+                req.getParameter("loginUser"),
+                req.getParameter("emailUser"),
+                new Timestamp(System.currentTimeMillis())
+        ));
+    }
+    /**
+     * The method handle a Put request.
+     */
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        this.users.updateUser(new User(
+                req.getParameter("nameUser"),
+                req.getParameter("loginUser"),
+                req.getParameter("emailUser"),
+                new Timestamp(System.currentTimeMillis())
+        ));
+    }
+    /**
+     * The method handle a Delete request.
+     */
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        this.users.removeUser(Integer.parseInt(req.getParameter("id")));
+    }
+    /**
+     * The method closes working with the database.
+     */
+    @Override
+    public void destroy() {
+        this.users.closeConnection();
+    }
+}
